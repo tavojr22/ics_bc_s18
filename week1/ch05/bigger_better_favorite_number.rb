4ec472e0f283ec4ebf7f0a75ab37a53e3199b826
tavojr22 <oseguera.jr22@gmail.com>
2018-06-18 19:42:46 -0700

    Complete week1 exercise

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+digit = gets.chomp
+bigger = digit.to_i + 1
+puts 'Thats an awful number. Change it to ' + bigger.to_s + ' instead.'
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello, may I know your first name?'
+name = gets.chomp
+puts 'Wonderful ' + name + ' ! What about middle name?'
+middle = gets.chomp
+puts name + ' ' + middle + ' ' + ', what a mixture! What\'s next? Oh that\'s right your last name.'
+last = gets.chomp
+puts 'Outstading work ' + name + ' ' + middle + ' ' + last + '! I am excited to see what awaits for us!'
